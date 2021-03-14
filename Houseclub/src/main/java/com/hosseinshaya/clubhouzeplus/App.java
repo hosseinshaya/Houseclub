328db9ab0ddaa4e3fb4d47a9d4e1d1fe9199334e
Hossein <ho3einsg@gmail.com>
2021-03-14 21:24:00 +0330

    change font

--- Houseclub/src/main/java/com/hosseinshaya/clubhouzeplus/App.java
+++ Houseclub/src/main/java/com/hosseinshaya/clubhouzeplus/App.java
@@ -4,7 +4,9 @@ import android.app.Application;
 import android.content.Context;
 
 import me.grishka.appkit.utils.V;
+
 import com.hosseinshaya.clubhouzeplus.api.ClubhouseSession;
+import com.hosseinshaya.clubhouzeplus.utils.TypefaceUtil;
 
 public class App extends Application {
     public static Context applicationContext;
@@ -12,6 +14,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        TypefaceUtil.overrideFont(getApplicationContext(), "SERIF", "fonts/IRANSansMobile.ttf");
         applicationContext = getApplicationContext();
         V.setApplicationContext(applicationContext);
         ClubhouseSession.load();
